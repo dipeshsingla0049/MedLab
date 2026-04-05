124e89f3de47febe8a7a4a5424ce5f9cce4bad10
Dipesh Singla <dipeshsingla2007@gmail.com>
2026-04-05 11:06:23 +0530

    Hero search added

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
-const url = 'https://69c16e00085e1a9fae410108.mockapi.io/api/v1/Hospitals';
+var API_URL = 'https://69c16e00085e1a9fae410108.mockapi.io/api/v1/Hospitals';
+var allHospitals = [];
 
-function renderHospitals() {
-    const grid = document.getElementById('hospital-grid');
-    
-    if (!grid) return;
-    
-    fetch(url)
-    .then(res => res.json())
-    .then(hospitals => {
-        hospitals.map(h => {
-            const card = document.createElement('div');
-            card.className = 'hospital-card';
-
-            const img = document.createElement('img');
-            img.src = h.image;
-
-            const name = document.createElement('h3');
-            name.innerText = h.name;
-
-            const city = document.createElement('p');
-            city.innerText = h.city;
-
-            const general = document.createElement('p');
-            general.innerText = 'General Beds: ' + h.generalBeds;
-
-            const icu = document.createElement('p');
-            icu.innerText = 'ICU Beds: ' + h.icuBeds;
-
-            const avail = document.createElement('p');
-            avail.innerText = 'Available: ' + h.availBeds;
-
-            const phone = document.createElement('p');
-            phone.innerText = h.phone;
-
-            card.append(img, name, city, general, icu, avail, phone);
-            grid.append(card);
-        });
-    })
-    .catch(err => console.error('Error fetching hospitals:', err));
-}
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.getElementById('hospital-grid')) {
-        renderHospitals();
+document.addEventListener('DOMContentLoaded',function(){
+    if(document.getElementById('hospital-grid')) {
+        initBedsPage();
     }
+    if (document.getElementById('favorites-grid')) {
+    initFavoritesPage();
+  }
+  if (document.getElementById('hero-search')) {
+    initHeroSearch();
+  }
 });
+function initHeroSearch() {
+    let input = document.getElementById('hero-search');
+    let btn = document.getElementById('btn-hero-search');
+    if(!btn || !input) {
+        return};
+    let handleSearch = function() {
+        let query = input.value.trim();
+        if (query) {
+            window.location.href = 'beds.html?q=' + encodeURIComponent(query);
+        } else {
+            window.location.href = 'beds.html';
+        }
+}
+btn.addEventListener('click', handleSearch);}
+
+
